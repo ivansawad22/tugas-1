a4f0f5d3b5d5fb159cc23eae695238027d77ac26
ivansawad22 <143860437+ivansawad22@users.noreply.github.com>
2023-09-05 11:20:49 +0700

    Add files via upload

---
+++ C TO F.py
@@ -0,0 +1,5 @@
+#konversi suhu dari celcius ke fahrenheit
+print("PROGRAM KONVERSI DARI CELCIUS KE FAHRENHEIT")
+C = float(input("masukkan suhu dalam celcius"))
+F = (9/5) * (C + 32)
+print("suhu dalam fahrenheit adalah", F, "fahrenheit")
---
+++ F TO C.py
@@ -0,0 +1,5 @@
+#program konversi suhu dari fahrenheit ke celcius
+print("PROGRAM KONVERSI DARI FAHRENHEIT KE CELCIUS")
+fa = float(input("masukkan suhu dalam fahrenheit"))
+celcius = (5/9) * fa - 32
+print("suhu dalam celcius adalah", celcius, "celcius")
